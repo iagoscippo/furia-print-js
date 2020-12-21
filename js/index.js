8e538819db01b5e37e239d39864ce01d37d091ce
iagoscippo <iagoscippo@gmail.com>
2020-12-21 16:17:45 -0300

    incorporreé uso básico de arrays

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
-function Disenio (colorIng, nombreIng) {
+function RemeraNueva (colorIng, nombreIng) {
     this.nombre = nombreIng
     this.color = colorIng;
-
+    
     this.setPrecio = () => {
         return precio;}
     this.getId = () => {return id;}
@@ -9,9 +9,26 @@ function Disenio (colorIng, nombreIng) {
 
 const nombre = prompt("ingresá el nombre de tu diseño");
 const color = prompt("en qué color? blanco/negro");
+let remeraNueva = new RemeraNueva(color, nombre);
+const items = [{
+    id: 1,
+    nombre: "remera1",
+    color: "blanco",
+    pecio: "$700",
+}, {
+    id: 2,
+    name: "remera2",
+    precio: "$800",
+    color: "gris"
+}, {
+    id: 3,
+    name: "remera3",
+    price: "$900",
+    color: "negro"
+}]
 
-let remeraNueva = new Disenio(color, nombre);
 
+// desafio objetos
 if (remeraNueva.color == "blanco") {
     alert("remera publicada a $700")
 } else if (remeraNueva.color == "negro") {
@@ -19,3 +36,13 @@ if (remeraNueva.color == "blanco") {
 } else {
     alert("hubo un problema, probablemente no elegiste ni blanco ni negro")
 };
+
+//desafío arrays
+function consultarPrecio () {
+    let consulta = prompt("ingrese el id para consultar el precio")
+    let item = items[consulta - 1];
+    return item.precio;
+}
+
+let itemConsultado = consultarPrecio();
+alert(itemConsultado);
